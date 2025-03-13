a131d5815444a1a91830b5a4c3dd297bd2e54473
JunilSeo <dccrdseo@naver.com>
2025-03-13 13:24:24 +0900

    feat: 할일 목록 Cell 생성

---
+++ ToDoCat/Views/ToDoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ToDoTableViewCell.swift
+//  ToDoCat
+//
+//  Created by 서준일 on 3/13/25.
+//
+
+import UIKit
+
+class ToDoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
