6cdd361cd539bb3e3f10cddc29f238e9251c75a3
JunilSeo <dccrdseo@naver.com>
2025-03-17 21:47:31 +0900

    fix: date 프로퍼티 삭제 후 생성 시간, 업데이트 시간 추가 및 Equatable 채택 후 == 생성

--- ToDoCat/Models/ToDoModel.swift
+++ ToDoCat/Models/ToDoModel.swift
@@ -7,16 +7,28 @@
 
 import UIKit
 
-struct ToDoItem {
+struct ToDoItem: Equatable {
     let id: UUID
-    let date: Date
     let content: String
     let image: UIImage?
+    var isCompleted: Bool = false
+    var createdAt: Date
+    var updatedAt: Date
     
-    init(id: UUID = UUID(), date: Date, content: String, image: UIImage? = nil) {
+    init(id: UUID = UUID(), content: String, image: UIImage? = nil, isCompleted: Bool = false, createdAt: Date = Date(), updatedAt: Date = Date()) {
         self.id = id
-        self.date = date
         self.content = content
         self.image = image
+        self.isCompleted = isCompleted
+        self.createdAt = createdAt
+        self.updatedAt = updatedAt
+    }
+    
+    static func == (lhs: ToDoItem, rhs: ToDoItem) -> Bool {
+        return lhs.content == rhs.content &&
+        lhs.image?.jpegData(compressionQuality: 0.7) == rhs.image?.jpegData(compressionQuality: 0.7) &&
+        lhs.isCompleted == rhs.isCompleted &&
+        lhs.createdAt == rhs.createdAt &&
+        lhs.updatedAt == rhs.updatedAt
     }
 }
