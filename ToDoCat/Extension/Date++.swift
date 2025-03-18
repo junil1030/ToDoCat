b75f0a689b67e821b5807fff7a88428a55960a8d
JunilSeo <dccrdseo@naver.com>
2025-03-18 17:02:00 +0900

    feat: UTC 시간을 KST로 바꾸기 위한 함수 추가

--- ToDoCat/Extension/Date++.swift
+++ ToDoCat/Extension/Date++.swift
@@ -10,14 +10,19 @@ import UIKit
 
 extension Date {
     
-    func toString(format: String = "yyyy-mm-dd") -> String {
+    func toString(format: String = "yyyy-MM-dd") -> String {
         let formatter = DateFormatter()
         formatter.dateFormat = format
+        formatter.timeZone = TimeZone(identifier: "Asis/Seoul")
         return formatter.string(from: self)
     }
     
     func isSameDay(as date: Date) -> Bool {
         let calendar = Calendar.current
         return calendar.isDate(self, inSameDayAs: date)
+    }
+
+    func toKST() -> Date {
+        return Calendar.current.date(byAdding: .second,value: Common.korTimeZone.secondsFromGMT(for: self), to: self)!
     }
 }
