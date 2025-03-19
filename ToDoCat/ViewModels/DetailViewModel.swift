c3d2c7590d4907154754e145798abf536d8a2a98
JunilSeo <dccrdseo@naver.com>
2025-03-19 16:13:15 +0900

    feat: 새롭게 작성하는 것과 기존 것 수정하는 enum 채택 후, init에서 변경 및 Data 추가하는 메서드 작성

--- ToDoCat/ViewModels/DetailViewModel.swift
+++ ToDoCat/ViewModels/DetailViewModel.swift
@@ -8,17 +8,50 @@ import UIKit
 
 class DetailViewModel {
     
+    enum Mode {
+        case new
+        case edit(ToDoItem)
+    }
+    
+    var mode: Mode
+    var selectedDate: Date
+    
+    var onDataUpdated: (() -> Void)?
+    var onDataAdded: (() -> Void)?
+    
+    var content: String {
+        didSet { onDataUpdated?() }
+    }
+    var titleImage: UIImage? {
+        didSet { onDataUpdated?() }
+    }
+    
+    init(mode: Mode, selectedDate: Date) {
+        self.mode = mode
+        self.selectedDate = selectedDate
+        switch mode {
+        case .new:
+            self.content = "할 일을 입력해보세요 !!"
+            self.titleImage = UIImage(systemName: "photo.badge.magnifyingglass")?.resized(to: CGSize(width: 40, height: 40))
+        case .edit(let todoItem):
+            self.content = todoItem.content
+            self.titleImage = todoItem.image
+            print("edit Init Complete")
+        }
+        
+        DispatchQueue.main.async { [weak self] in
+            self?.onDataUpdated?()
+        }
+    }
     
     // 새 항목 추가 메서드
-    func addEntry(content: String, image: UIImage? = nil, isCompleted: Bool = false, createdAt: Date = Date(), updatedAt: Date = Date()) {
-        let newToDo = ToDoItem(
-            id: UUID()
-            , content: content
-            , image: image ?? createSampleImage(withColor: .systemBlue)
-            , isCompleted: isCompleted
-            , createdAt: createdAt
-            , updatedAt: updatedAt)
-        
+    func addEntry(newToDo: ToDoItem) {
+        if ToDoDataManager.shared.createToDo(todoItem: newToDo) {
+            print("저장 완료")
+            onDataAdded?()
+        } else {
+            fatalError("저장에 실패했습니다. 잠시후 다시 시도해주세요.")
+        }
     }
     
     // 샘플 이미지 생성 (실제 앱에서는 사용자가 제공하는 이미지 사용)
