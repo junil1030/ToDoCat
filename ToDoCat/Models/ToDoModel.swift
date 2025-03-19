1fdcc95601cf1aed6d6ee9b75452ed589595916f
JunilSeo <dccrdseo@naver.com>
2025-03-19 16:04:43 +0900

    feat: ToDo Data 모델 속성 추가

--- ToDoCat/Models/ToDoModel.swift
+++ ToDoCat/Models/ToDoModel.swift
@@ -12,14 +12,16 @@ struct ToDoItem: Equatable {
     let content: String
     let image: UIImage?
     var isCompleted: Bool = false
+    var date: Date
     var createdAt: Date
     var updatedAt: Date
     
-    init(id: UUID = UUID(), content: String, image: UIImage? = nil, isCompleted: Bool = false, createdAt: Date = Date(), updatedAt: Date = Date()) {
+    init(id: UUID = UUID(), content: String, image: UIImage? = nil, isCompleted: Bool = false, date: Date, createdAt: Date = Date(), updatedAt: Date = Date()) {
         self.id = id
         self.content = content
         self.image = image
         self.isCompleted = isCompleted
+        self.date = date
         self.createdAt = createdAt
         self.updatedAt = updatedAt
     }
@@ -28,6 +30,7 @@ struct ToDoItem: Equatable {
         return lhs.content == rhs.content &&
         lhs.image?.jpegData(compressionQuality: 0.7) == rhs.image?.jpegData(compressionQuality: 0.7) &&
         lhs.isCompleted == rhs.isCompleted &&
+        lhs.date == rhs.date &&
         lhs.createdAt == rhs.createdAt &&
         lhs.updatedAt == rhs.updatedAt
     }
