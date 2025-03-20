fb088deb858dcb7e8f35a72b3bbcdf2672ff6329
JunilSeo <dccrdseo@naver.com>
2025-03-21 00:33:08 +0900

    feat: SearchBarController 네비게이션컨트롤러 생성 및 탭바 추가

--- ToDoCat/Views/Controllers/MainTabBarController.swift
+++ ToDoCat/Views/Controllers/MainTabBarController.swift
@@ -8,20 +8,24 @@
 import UIKit
 
 class MainTabBarController: UITabBarController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         setupTabs()
     }
     
-
+    
     func setupTabs() {
         let homeVC = HomeViewController()
         let homeNav = UINavigationController(rootViewController: homeVC)
         homeNav.tabBarItem = UITabBarItem(title: "", image: UIImage(systemName: "square.and.pencil"), tag: 0)
         
-        viewControllers = [homeNav]
+        let searchVC = SearchViewController()
+        let searchNav = UINavigationController(rootViewController: searchVC)
+        searchNav.tabBarItem = UITabBarItem(title: "", image: UIImage(systemName: "magnifyingglass"), tag: 1)
+        
+        viewControllers = [homeNav, searchNav]
     }
-
+    
 }
