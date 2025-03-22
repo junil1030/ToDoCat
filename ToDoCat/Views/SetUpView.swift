c500ff696117293a5800bf16a299d6401cdc9b35
JunilSeo <dccrdseo@naver.com>
2025-03-22 15:19:52 +0900

    :feat: 설정 탭 생성

---
+++ ToDoCat/Views/SetUpView.swift
@@ -0,0 +1,17 @@
+//
+//  SetUpView.swift
+//  ToDoCat
+//
+//  Created by 서준일 on 3/22/25.
+//
+
+import UIKit
+
+class SetUpView: UIView {
+
+    lazy var profileView: UIView = {
+        let uiView = UIView()
+        
+        return uiView
+    }()
+}
