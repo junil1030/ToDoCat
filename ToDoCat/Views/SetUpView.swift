bd73f7b30471edf9ea139cb8312e118faae522de
JunilSeo <dccrdseo@naver.com>
2025-03-24 14:18:18 +0900

    feat: tableView 생성 및 그룹화

--- ToDoCat/Views/SetUpView.swift
+++ ToDoCat/Views/SetUpView.swift
@@ -8,10 +8,32 @@
 import UIKit
 
 class SetUpView: UIView {
-
-    lazy var profileView: UIView = {
-        let uiView = UIView()
-        
-        return uiView
+    
+    let tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
     }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+    
+    private func setupView() {
+        backgroundColor = .systemBackground
+        addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
 }
