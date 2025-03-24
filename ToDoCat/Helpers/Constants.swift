cca49f36d022719178bd1f16980da2efffbadd16
JunilSeo <dccrdseo@naver.com>
2025-03-24 17:59:52 +0900

    앱 버전과 디바이스 정보 불러오는 함수 추가

--- ToDoCat/Helpers/Constants.swift
+++ ToDoCat/Helpers/Constants.swift
@@ -16,4 +16,32 @@ struct Constants {
         }
         return "https://cataas.com/cat/says/\(says)?width=400&height=400"
     }
+    
+    static func getAppVersion() -> String {
+        return Bundle.main.infoDictionary?["CFBundleShortVersionString"] as! String
+    }
+    
+    static func getDeviceIdentifier() -> String {
+        var systemInfo = utsname()
+        uname(&systemInfo)
+        let machineMirror = Mirror(reflecting: systemInfo.machine)
+        let identifier = machineMirror.children.reduce("") { identifier, element in
+            guard let value = element.value as? Int8, value != 0 else { return identifier }
+            return identifier + String(UnicodeScalar(UInt8(value)))
+        }
+        
+        return identifier
+    }
+    
+    static func getDeviceModelName() -> String {
+        let device = UIDevice.current
+        let selName = "_\("deviceInfo")ForKey:"
+        let selector = NSSelectorFromString(selName)
+        
+        if device.responds(to: selector) { // [옵셔널 체크 실시]
+            let modelName = String(describing: device.perform(selector, with: "marketing-name").takeRetainedValue())
+            return modelName
+        }
+        return "알 수 없음"
+    }
 }
