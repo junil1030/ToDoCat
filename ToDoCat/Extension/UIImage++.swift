ba5249099d32fb661633690f6a018640baddcdc6
JunilSeo <dccrdseo@naver.com>
2025-03-28 16:37:55 +0900

    feat: MaxSize로 변경하는 함수 추가

--- ToDoCat/Extension/UIImage++.swift
+++ ToDoCat/Extension/UIImage++.swift
@@ -14,6 +14,19 @@ extension UIImage {
         }
     }
     
+    func resizedToMaxSize(maxWidth: CGFloat, maxHeight: CGFloat) -> UIImage {
+        if self.size.width <= maxWidth && self.size.height <= maxHeight {
+            return self
+        }
+        
+        let aspectWidth = maxWidth / self.size.width
+        let aspectHeight = maxHeight / self.size.height
+        let aspectRatio = min(aspectWidth, aspectHeight)
+        let newSize = CGSize(width: self.size.width * aspectRatio, height: self.size.height * aspectRatio)
+        
+        return resized(to: newSize)
+    }
+    
     func compressed(quality: CGFloat = 0.7) -> UIImage? {
         guard let data = jpegData(compressionQuality: quality) else { return nil }
         return UIImage(data: data)
