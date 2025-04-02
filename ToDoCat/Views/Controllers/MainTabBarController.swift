15405bd040452d43bb7a5f9e8d8c824191382e69
JunilSeo <dccrdseo@naver.com>
2025-04-02 21:46:46 +0900

    fix: controller 채택 함수 삭제

--- ToDoCat/Views/Controllers/MainTabBarController.swift
+++ ToDoCat/Views/Controllers/MainTabBarController.swift
@@ -14,7 +14,6 @@ class MainTabBarController: UITabBarController {
         
         setupAppearance()
         setupColors()
-        setupTabs()
     }
     
     func setupAppearance() {
@@ -37,21 +36,4 @@ class MainTabBarController: UITabBarController {
     func setupColors() {
         self.tabBar.tintColor = UIColor(named: "CalendarColor")
     }
-    
-    func setupTabs() {
-        let homeVC = HomeViewController()
-        let homeNav = UINavigationController(rootViewController: homeVC)
-        homeNav.tabBarItem = UITabBarItem(title: "", image: UIImage(systemName: "square.and.pencil"), tag: 0)
-        
-        let searchVC = SearchViewController()
-        let searchNav = UINavigationController(rootViewController: searchVC)
-        searchNav.tabBarItem = UITabBarItem(title: "", image: UIImage(systemName: "magnifyingglass"), tag: 1)
-        
-        let settingsVC = SettingsViewController()
-        let settingsNav = UINavigationController(rootViewController: settingsVC)
-        settingsNav.tabBarItem = UITabBarItem(title: "", image: UIImage(systemName: "gearshape"), tag: 2)
-        
-        viewControllers = [homeNav, searchNav, settingsNav]
-    }
-    
 }
