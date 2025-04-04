0217a0968bb134dcadd1928d4958134536f1308b
JunilSeo <dccrdseo@naver.com>
2025-04-04 20:47:21 +0900

    refactor: 함수 비동기화 후 콜백함수 추가 및 에러 타입 재정의, 비동기 함수 전체 리팩토링

--- ToDoCat/Data/ToDoDataManager.swift
+++ ToDoCat/Data/ToDoDataManager.swift
@@ -18,80 +18,118 @@ protocol RealmManageable {
     func delete<T: Object>(_ object: T) throws
     func deleteAll<T: Object>(_ objects: Results<T>) throws
     func observeChanges<T: Object>(for type: T.Type, completion: @escaping (RealmCollectionChange<Results<T>>) -> Void) -> NotificationToken
+    
+    func performBackgroundTask<T>(_ task: @escaping () -> T, completion: @escaping (T) -> Void)
+    func createThreadSafeReference<T: ThreadConfined>(for object: T) -> ThreadSafeReference<T>
+    func resolve<T: ThreadConfined>(_ reference: ThreadSafeReference<T>) -> T?
 }
 
 // MARK: - Realm 구현
 class RealmManager: RealmManageable {
-    private let realm: Realm
+    private let configuration: Realm.Configuration
     
     init(configuration: Realm.Configuration = Realm.Configuration(schemaVersion: 1)) throws {
-        realm = try Realm(configuration: configuration)
+        self.configuration = configuration
         print("Realm 위치: \(configuration.fileURL?.absoluteString ?? "Unknown")")
     }
     
+    private func getRealm() -> Realm {
+        do {
+            return try Realm(configuration: configuration)
+        } catch {
+            fatalError("Realm 인스턴스 생성 실패: \(error)")
+        }
+    }
+    
     func getObject<T: Object>(ofType type: T.Type, forPrimaryKey key: String) -> T? {
-        return realm.object(ofType: type, forPrimaryKey: key)
+        return getRealm().object(ofType: type, forPrimaryKey: key)
     }
     
     func getObjects<T: Object>(_ type: T.Type) -> Results<T> {
-        return realm.objects(type)
+        return getRealm().objects(type)
     }
     
     func getFilteredObjects<T: Object>(_ type: T.Type, predicate: NSPredicate) -> Results<T> {
-        return realm.objects(type).filter(predicate)
+        return getRealm().objects(type).filter(predicate)
     }
     
     func getSortedObjects<T: Object>(_ type: T.Type, sortedByKeyPath keyPath: String, ascending: Bool) -> Results<T> {
-        return realm.objects(type).sorted(byKeyPath: keyPath, ascending: ascending)
+        return getRealm().objects(type).sorted(byKeyPath: keyPath, ascending: ascending)
     }
     
     func write<T>(_ block: () throws -> T) throws -> T {
-        try realm.write(block)
+        let realm = getRealm()
+        return try realm.write(block)
     }
     
     func add<T: Object>(_ object: T, update: Realm.UpdatePolicy = .modified) throws {
+        let realm = getRealm()
         try realm.write {
             realm.add(object, update: update)
         }
     }
     
     func delete<T: Object>(_ object: T) throws {
+        let realm = getRealm()
         try realm.write {
             realm.delete(object)
         }
     }
     
     func deleteAll<T: Object>(_ objects: Results<T>) throws {
+        let realm = getRealm()
         try realm.write {
             realm.delete(objects)
         }
     }
     
     func observeChanges<T: Object>(for type: T.Type, completion: @escaping (RealmCollectionChange<Results<T>>) -> Void) -> NotificationToken {
-        let results = realm.objects(type)
+        let results = getRealm().objects(type)
         return results.observe(completion)
+    }
+    
+    // 백그라운드 작업을 위한 메서드
+    func performBackgroundTask<T>(_ task: @escaping () -> T, completion: @escaping (T) -> Void) {
+        DispatchQueue.global(qos: .userInitiated).async {
+            autoreleasepool {
+                let result = task()
+                
+                DispatchQueue.main.async {
+                    completion(result)
+                }
+            }
+        }
+    }
+    
+    func createThreadSafeReference<T: ThreadConfined>(for object: T) -> ThreadSafeReference<T> {
+        return ThreadSafeReference(to: object)
+    }
+    
+    func resolve<T: ThreadConfined>(_ reference: ThreadSafeReference<T>) -> T? {
+        let realm = getRealm()
+        return realm.resolve(reference)
     }
 }
 
 //MARK: - ToDoData Protocol
 protocol ToDoDataFetchable {
-    func getAllToDos() ->[ToDoItem]
-    func getToDos(forDate date: Date) -> [ToDoItem]
+    func getAllToDos(completion: @escaping ([ToDoItem]) -> Void)
+    func getToDos(forDate date: Date, completion: @escaping ([ToDoItem]) -> Void)
 }
 
 protocol ToDoDataSearchable {
-    func searchToDos(keyword: String) -> [ToDoItem]
+    func searchToDos(keyword: String, completion: @escaping ([ToDoItem]) -> Void)
 }
 
 protocol ToDoDataEditable {
-    func createToDo(todoItem: ToDoItem) -> Result<Void, Error>
-    func updateToDo(id: UUID, content: String, image: UIImage?) -> Result<Void, Error>
-    func toggleCompletion(id: UUID) -> Result<Void, Error>
+    func createToDo(todoItem: ToDoItem, completion: @escaping (Result<Void, Error>) -> Void)
+    func updateToDo(id: UUID, content: String, image: UIImage?, completion: @escaping (Result<Void, Error>) -> Void)
+    func toggleCompletion(id: UUID, completion: @escaping (Result<Void, Error>) -> Void)
 }
 
 protocol ToDoDataDeletable {
-    func deleteToDo(id: UUID) -> Result<Void, Error>
-    func deleteToDoAll() -> Result<Void, Error>
+    func deleteToDo(id: UUID, completion: @escaping (Result<Void, Error>) -> Void)
+    func deleteToDoAll(completion: @escaping (Result<Void, Error>) -> Void)
 }
 
 protocol ToDoDataObserver {
@@ -137,110 +175,162 @@ class ToDoDataManager: ToDoDataObserver {
 
 //MARK: - ToDoDataFetchableProtocol
 extension ToDoDataManager: ToDoDataFetchable {
-    func getAllToDos() -> [ToDoItem] {
-        let realmResults = realmManager.getSortedObjects(ToDoItemRealm.self, sortedByKeyPath: "date", ascending: true)
-        
-        return realmResults.map { $0.toToDoItem() }
+    func getAllToDos(completion: @escaping ([ToDoItem]) -> Void) {
+        realmManager.performBackgroundTask {
+            let realmResults = self.realmManager.getSortedObjects(ToDoItemRealm.self, sortedByKeyPath: "date", ascending: true)
+            return realmResults.map { $0.toToDoItem() }
+        } completion: { todoItems in
+            completion(todoItems)
+        }
     }
     
-    func getToDos(forDate date: Date) -> [ToDoItem] {
-        let calendar = Calendar.current
-        let startOfDay = calendar.startOfDay(for: date)
-        let endOfDay = calendar.date(byAdding: .day, value: 1, to: startOfDay)!
-        
-        let predicate = NSPredicate(format: "date >= %@ AND date < %@", startOfDay as NSDate, endOfDay as NSDate)
-        let realmItems = realmManager.getFilteredObjects(ToDoItemRealm.self, predicate: predicate)
-        
-        return realmItems.map { $0.toToDoItem() }
+    func getToDos(forDate date: Date, completion: @escaping ([ToDoItem]) -> Void) {
+        realmManager.performBackgroundTask {
+            let calendar = Calendar.current
+            let startOfDay = calendar.startOfDay(for: date)
+            let endOfDay = calendar.date(byAdding: .day, value: 1, to: startOfDay)!
+            
+            let predicate = NSPredicate(format: "date >= %@ AND date < %@", startOfDay as NSDate, endOfDay as NSDate)
+            let realmItems = self.realmManager.getFilteredObjects(ToDoItemRealm.self, predicate: predicate)
+            
+            return realmItems.map { $0.toToDoItem() }
+        } completion: { todoItems in
+            completion(todoItems)
+        }
     }
 }
 
 //MARK: - ToDoDataSearchableProtocol
 extension ToDoDataManager: ToDoDataSearchable {
-    func searchToDos(keyword: String) -> [ToDoItem] {
-        let predicate = NSPredicate(format: "content CONTAINS[cd] %@", keyword)
-        let realmItems = realmManager.getFilteredObjects(ToDoItemRealm.self, predicate: predicate)
-        
-        return realmItems.map { $0.toToDoItem() }
+    func searchToDos(keyword: String, completion: @escaping ([ToDoItem]) -> Void) {
+        realmManager.performBackgroundTask {
+            let predicate = NSPredicate(format: "content CONTAINS[cd] %@", keyword)
+            let realmItems = self.realmManager.getFilteredObjects(ToDoItemRealm.self, predicate: predicate)
+            
+            return realmItems.map { $0.toToDoItem() }
+        } completion: { todoItems in
+            completion(todoItems)
+        }
     }
 }
 
 //MARK: - ToDoDataEditableProtocol
 extension ToDoDataManager: ToDoDataEditable {
-    func createToDo(todoItem: ToDoItem) -> Result<Void, Error> {
-        do {
-            let realmItem = ToDoItemRealm(toDoItem: todoItem)
-            try realmManager.add(realmItem, update: .modified)
-            return .success(())
-        } catch {
-            return .failure(ToDoDataError.realmError(error))
+    func createToDo(todoItem: ToDoItem, completion: @escaping (Result<Void, Error>) -> Void) {
+        realmManager.performBackgroundTask {
+            do {
+                let realmItem = ToDoItemRealm(toDoItem: todoItem)
+                try self.realmManager.add(realmItem, update: .modified)
+                return Result<Void, Error>.success(())
+            } catch {
+                return Result<Void, Error>.failure(ToDoDataError.realmError(error))
+            }
+        } completion: { result in
+            completion(result)
         }
     }
     
-    func updateToDo(id: UUID, content: String, image: UIImage?) -> Result<Void, Error> {
-        do {
-            guard let todoToUpdate = realmManager.getObject(ofType: ToDoItemRealm.self, forPrimaryKey: id.uuidString) else {
-                return .failure(ToDoDataError.itemNotFound)
+    func updateToDo(id: UUID, content: String, image: UIImage?, completion: @escaping (Result<Void, Error>) -> Void) {
+        // 먼저 메인 스레드에서 객체를 찾기
+        guard let todoToUpdate = realmManager.getObject(ofType: ToDoItemRealm.self, forPrimaryKey: id.uuidString) else {
+            completion(.failure(ToDoDataError.itemNotFound))
+            return
+        }
+        
+        // ThreadSafeReference 생성
+        let reference = realmManager.createThreadSafeReference(for: todoToUpdate)
+        
+        // 이미지 데이터 미리 처리
+        let imageData = image?.jpegData(compressionQuality: 0.7)
+        
+        // 백그라운드에서 처리
+        realmManager.performBackgroundTask {
+            guard let todoItem = self.realmManager.resolve(reference) else {
+                return Result<Void, Error>.failure(ToDoDataError.itemNotFound)
             }
             
-            try realmManager.write {
-                todoToUpdate.content = content
-                
-                if let newImage = image {
-                    todoToUpdate.imageData = newImage.jpegData(compressionQuality: 0.7)
-                } else {
-                    todoToUpdate.imageData = nil
+            do {
+                try self.realmManager.write {
+                    todoItem.content = content
+                    todoItem.imageData = imageData
+                    todoItem.updatedAt = Date()
                 }
-                
-                todoToUpdate.updatedAt = Date()
+                return Result<Void, Error>.success(())
+            } catch {
+                return Result<Void, Error>.failure(ToDoDataError.realmError(error))
             }
-            
-            return .success(())
-        } catch {
-            return .failure(ToDoDataError.realmError(error))
+        } completion: { result in
+            completion(result)
         }
     }
     
-    func toggleCompletion(id: UUID) -> Result<Void, Error> {
-        do {
-            guard let todoToUpdate = realmManager.getObject(ofType: ToDoItemRealm.self, forPrimaryKey: id.uuidString) else {
-                return .failure(ToDoDataError.itemNotFound)
-            }
-            
-            try realmManager.write {
-                todoToUpdate.isCompleted = !todoToUpdate.isCompleted
-                todoToUpdate.updatedAt = Date()
-            }
-            
-            return .success(())
-        } catch {
-            return .failure(ToDoDataError.realmError(error))
+    func toggleCompletion(id: UUID, completion: @escaping (Result<Void, Error>) -> Void) {
+        guard let todoToUpdate = realmManager.getObject(ofType: ToDoItemRealm.self, forPrimaryKey: id.uuidString) else {
+            completion(.failure(ToDoDataError.itemNotFound))
+            return
         }
-    }
-}
+        
+        // ThreadSafeReference 생성
+        let reference = realmManager.createThreadSafeReference(for: todoToUpdate)
+        let currentCompletionState = todoToUpdate.isCompleted
+        
+        realmManager.performBackgroundTask {
+            guard let todoItem = self.realmManager.resolve(reference) else {
+                return Result<Void, Error>.failure(ToDoDataError.itemNotFound)
+            }
+            
+            do {
+                try self.realmManager.write {
+                    todoItem.isCompleted = !currentCompletionState
+                    todoItem.updatedAt = Date()
+                }
+                return Result<Void, Error>.success(())
+            } catch {
+                return Result<Void, Error>.failure(ToDoDataError.realmError(error))
+            }
+        } completion: { result in
+            completion(result)
+        }
+    }}
 
 //MARK: - ToDoDeletableProtocol
 extension ToDoDataManager: ToDoDataDeletable {
-    func deleteToDo(id: UUID) -> Result<Void, Error> {
-        do {
-            guard let todoToDelete = realmManager.getObject(ofType: ToDoItemRealm.self, forPrimaryKey: id.uuidString) else {
-                return .failure(ToDoDataError.itemNotFound)
+    func deleteToDo(id: UUID, completion: @escaping (Result<Void, Error>) -> Void) {
+        guard let todoToDelete = realmManager.getObject(ofType: ToDoItemRealm.self, forPrimaryKey: id.uuidString) else {
+            completion(.failure(ToDoDataError.itemNotFound))
+            return
+        }
+        
+        // ThreadSafeReference 생성
+        let reference = realmManager.createThreadSafeReference(for: todoToDelete)
+        
+        realmManager.performBackgroundTask {
+            guard let todoItem = self.realmManager.resolve(reference) else {
+                return Result<Void, Error>.failure(ToDoDataError.itemNotFound)
             }
             
-            try realmManager.delete(todoToDelete)
-            return .success(())
-        } catch {
-            return .failure(ToDoDataError.realmError(error))
+            do {
+                try self.realmManager.delete(todoItem)
+                return Result<Void, Error>.success(())
+            } catch {
+                return Result<Void, Error>.failure(ToDoDataError.realmError(error))
+            }
+        } completion: { result in
+            completion(result)
         }
     }
     
-    func deleteToDoAll() -> Result<Void, Error> {
-        do {
-            let todosToDelete = realmManager.getObjects(ToDoItemRealm.self)
-            try realmManager.deleteAll(todosToDelete)
-            return .success(())
-        } catch {
-            return .failure(ToDoDataError.realmError(error))
+    func deleteToDoAll(completion: @escaping (Result<Void, Error>) -> Void) {
+        realmManager.performBackgroundTask {
+            do {
+                let todosToDelete = self.realmManager.getObjects(ToDoItemRealm.self)
+                try self.realmManager.deleteAll(todosToDelete)
+                return Result<Void, Error>.success(())
+            } catch {
+                return Result<Void, Error>.failure(ToDoDataError.realmError(error))
+            }
+        } completion: { result in
+            completion(result)
         }
     }
 }
