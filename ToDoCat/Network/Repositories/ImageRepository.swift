1fb417306874e77c8e08401f72fd5baf6784f8a6
JunilSeo <dccrdseo@naver.com>
2025-04-15 16:58:28 +0900

    refactor: 기존 fetch함수를 Rx를 사용하여 가져오도록 변경

--- ToDoCat/Network/Repositories/ImageRepository.swift
+++ ToDoCat/Network/Repositories/ImageRepository.swift
@@ -6,20 +6,37 @@
 //
 import Foundation
 import Alamofire
+import RxSwift
 
 protocol ImageRepositoryProtocol {
-    func fetchImage(from url: URL, completion: @escaping (Result<ImageEntity, Error>) -> Void)
+    func fetchImage(from url: String) -> Single<ImageEntity>
 }
 
 class ImageRepository: ImageRepositoryProtocol {
-    func fetchImage(from url: URL, completion: @escaping (Result<ImageEntity, Error>) -> Void) {
-        AF.request(url).responseData { response in
-            switch response.result {
-            case .success(let data):
-                let entity = ImageEntity(imageData: data)
-                completion(.success(entity))
-            case .failure(let error):
-                completion(.failure(error))
+    
+    func fetchImage(from url: String) -> Single<ImageEntity> {
+        return Single<ImageEntity>.create { observer in
+            // url 검증 단계
+            guard let iamgeURL = URL(string: url) else {
+                observer(.failure(NetworkError.invalidURL))
+                return Disposables.create()
+            }
+            
+            // 요청
+            let request = AF.request(iamgeURL)
+                .validate()
+                .responseData { response in
+                    switch response.result {
+                    case .success(let data):
+                        let imageEntity = ImageEntity(imageData: data)
+                        observer(.success(imageEntity))
+                    case .failure(let error):
+                        observer(.failure(NetworkError.networkError(error)))
+                    }
+                }
+            
+            return Disposables.create {
+                request.cancel()
             }
         }
     }
